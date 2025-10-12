a1cf754d6bd1dfef5c82cb0df0dab3ef48e8c144
leviutima <levi.utima@gmail.com>
2025-10-12 11:13:29 -0300

    feat

--- front/src/app/br/(private)/enterprise/[id]/modules.tsx
+++ front/src/app/br/(private)/enterprise/[id]/modules.tsx
@@ -4,7 +4,7 @@ export function ModuleSection() {
   return (
     <section className="mb-10 w-[80vw]">
       <div className="flex items-center justify-between mb-3">
-        <h2 className="text-xl font-semibold">Módulos</h2>
+        <h2 className="text-xl font-semibold">Cursos</h2>
         <div>
           <span className="underline text-blue-700 cursor-pointer hover:text-neutral-800">
             Ver todos
