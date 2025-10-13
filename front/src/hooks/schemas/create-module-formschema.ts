ce70c70ec8512021e3fa9354f3192ded3eda7281
Jhonatan Sampaio <jhonatan.sampaiof@gmail.com>
2025-10-13 09:40:33 -0300

    bugfix module create

--- front/src/hooks/schemas/create-module-formschema.ts
+++ front/src/hooks/schemas/create-module-formschema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const createModuleFormSchema = z.object({
   title: z.string(),
   description: z.string(),
-  enterpriseId: z.string(),
+  enterpriseId: z.string().optional(),
 });
 
 export type CreateModuleFormSchema = z.infer<typeof createModuleFormSchema>
