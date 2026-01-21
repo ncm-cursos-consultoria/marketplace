0fd6d7f2fdbdec163c2773402f99cc55778e169c
Jhonatan Sampaio <jhonatan.sampaiof@gmail.com>
2026-01-21 16:01:54 -0300

    bugfix weekly email sent candidate

--- api/marketplace/src/main/java/com/ncm/marketplace/usecases/services/command/user/candidate/UserCandidateCommandService.java
+++ api/marketplace/src/main/java/com/ncm/marketplace/usecases/services/command/user/candidate/UserCandidateCommandService.java
@@ -2,6 +2,7 @@ package com.ncm.marketplace.usecases.services.command.user.candidate;
 
 import com.ncm.marketplace.domains.user.candidate.UserCandidate;
 import com.ncm.marketplace.gateways.repositories.domains.user.candidate.UserCandidateRepository;
+import com.ncm.marketplace.usecases.services.query.user.candidate.UserCandidateQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserCandidateCommandService {
     private final UserCandidateRepository userCandidateRepository;
+    private final UserCandidateQueryService userCandidateQueryService;
 
     public UserCandidate save(UserCandidate userCandidate) {
         return userCandidateRepository.save(userCandidate);
@@ -18,5 +20,18 @@ public class UserCandidateCommandService {
 
     public void deleteById(String id) {
         userCandidateRepository.deleteById(id);
+    }
+
+    public UserCandidate saveAndFlush(UserCandidate userCandidate) {
+        return userCandidateRepository.saveAndFlush(userCandidate);
+    }
+
+    public void updateProfileAndEmailStatus(String id, Boolean finished, Boolean emailSent) {
+        UserCandidate candidate = userCandidateQueryService.findByIdOrThrow(id);
+        candidate.setFinishedProfile(finished);
+        if (emailSent) {
+            candidate.setIsWeeklyEmailSent(true);
+        }
+        userCandidateRepository.saveAndFlush(candidate);
     }
 }
--- api/marketplace/src/main/java/com/ncm/marketplace/usecases/services/scheduled/UserScheduledService.java
+++ api/marketplace/src/main/java/com/ncm/marketplace/usecases/services/scheduled/UserScheduledService.java
@@ -1,6 +1,7 @@
 package com.ncm.marketplace.usecases.services.scheduled;
 
 import com.ncm.marketplace.domains.user.candidate.UserCandidate;
+import com.ncm.marketplace.usecases.services.command.user.candidate.UserCandidateCommandService;
 import com.ncm.marketplace.usecases.services.email.EmailService;
 import com.ncm.marketplace.usecases.services.query.user.candidate.UserCandidateQueryService;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -20,31 +20,37 @@ public class UserScheduledService {
 
     private final UserCandidateQueryService userCandidateQueryService;
     private final EmailService emailService;
+    private final UserCandidateCommandService userCandidateCommandService;
 
-    @Transactional
     @Scheduled(cron = "0 0 9 * * *", zone = "America/Sao_Paulo")
     public void checkFinishedProfiles() throws IOException {
         List<UserCandidate> unfinishedProfileCandidates = userCandidateQueryService.findAllByFinishedProfiles(Boolean.FALSE);
-
+        Integer counter = 0;
+        Integer limit = 200;
+        Boolean profileFinished;
+        Boolean weeklyEmailSent = Boolean.FALSE;
         for (UserCandidate userCandidate : unfinishedProfileCandidates) {
-            if (isProfileFinished(userCandidate)) {
-                userCandidate.setFinishedProfile(Boolean.TRUE);
+            if (counter >= limit) break;
+            profileFinished = isProfileFinished(userCandidate);
+            if (profileFinished) {
                 log.info("User id {} profile finished", userCandidate.getId());
             } else {
-                userCandidate.setFinishedProfile(Boolean.FALSE);
                 log.info("User id {} profile unfinished", userCandidate.getId());
                 if (userCandidate.getReceiveEmail() && !userCandidate.getIsWeeklyEmailSent()) {
                     try {
                         log.info("Sending finish profile email to user id {}", userCandidate.getId());
                         emailService.sendFinishProfileEmail(userCandidate.getEmail(), userCandidate.getFullName());
-                        userCandidate.setIsWeeklyEmailSent(Boolean.TRUE);
+                        weeklyEmailSent = Boolean.TRUE;
+                        counter++;
                     } catch (Exception e) {
                         log.error("Não foi possível enviar e-mail para o usuário {}: {}", userCandidate.getId(), e.getMessage());
                     }
                 }
             }
+            if (profileFinished || weeklyEmailSent) {
+                userCandidateCommandService.updateProfileAndEmailStatus(userCandidate.getId(), profileFinished, weeklyEmailSent);
+            }
         }
-
     }
 
     public Boolean isProfileFinished(UserCandidate userCandidate) {
